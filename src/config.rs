2bb981e6c24e2e8c94da2978ede907aafaa9fa8b
ENIX1701 <enix1701@gmail.com>
2026-02-17 19:24:34 +0100

    chore: seaorm and env dependency and basic setup

--- src/config.rs
+++ src/config.rs
@@ -1,2 +1,16 @@
-// config xd
-pub fn load() {}
+use dotenvy::dotenv;
+use std::env;
+
+pub struct Config {
+    pub database_url: String,
+}
+
+impl Config {
+    pub fn init() -> Self {
+        dotenv().ok();
+
+        let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set in .env");
+
+        Config { database_url }
+    }
+}
