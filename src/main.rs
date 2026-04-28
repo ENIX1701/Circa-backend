4706b82af2f801647b28e6e723ebbf3558a829a7
ENIX1701 <enix1701@gmail.com>
2026-04-28 13:12:47 +0200

    chore: configurable port

--- src/main.rs
+++ src/main.rs
@@ -24,13 +24,15 @@ async fn main() -> std::io::Result<()> {
         .await
         .expect("Failed to connect to the database :c");
 
+    let port = std::env::var("PORT").ok().and_then(|value| value.parse::<u16>().ok()).unwrap_or(8080);
+
     let user_service = web::Data::new(UserService::new(UserRepository::new(db_conn.clone())));
     let event_service = web::Data::new(EventService::new(EventRepository::new(db_conn.clone())));
     let config = web::Data::new(config);
     let delivery = web::Data::new(delivery);
     let db_data = web::Data::new(db_conn);
 
-    println!("Server starting at 0.0.0.0:8080");
+    println!("Server starting at 0.0.0.0:{port}");
 
     HttpServer::new(move || {
         App::new()
@@ -48,7 +50,7 @@ async fn main() -> std::io::Result<()> {
             .service(Files::new("/assets", "./public/assets"))
             .default_service(web::get().to(spa_index))
     })
-    .bind(("0.0.0.0", 8080))?
+    .bind(("0.0.0.0", port))?
     .run()
     .await
 }
